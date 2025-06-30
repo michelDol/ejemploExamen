28da053c59db8c24732ce3fee152f229e03fd716
Michel Dolores <michel.dolores@hybridostudio.mx>
2025-06-29 20:57:26 -0600

    init commit

--- src/app.ts
+++ src/app.ts
@@ -2,5 +2,5 @@ export function calculateTax(amount: number, rate: number): number {
   if (amount < 0 || rate < 0) {
     throw new Error("Los valores no pueden ser negativos");
   }
-  return +(amount + rate).toFixed(2);
+  return +(amount * rate).toFixed(2);
 }
